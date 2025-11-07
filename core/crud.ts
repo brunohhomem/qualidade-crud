a987b4e62ac7d1e0c68dd1fe39c797eafd1637b4
Bruno H. Homem <brunohhomem@gmail.com>
2025-11-07 14:49:21 -0300

    feat: add `update`

--- core/crud.ts
+++ core/crud.ts
@@ -1,15 +1,18 @@
 import fs from "fs"
+import { v4 as uuid } from 'uuid'
 
 const DB_FILE_PATH = './core/db'
 
 interface Todo {
+  id: string
   date: string
   content: string
   done: boolean
 }
 
-function create(content: string) {
+function create(content: string): Todo {
   const todo: Todo = {
+    id: uuid(),
     date: new Date().toISOString(),
     content: content,
     done: false
@@ -23,7 +26,7 @@ function create(content: string) {
   fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
     todos
   }, null, 2))
-  return content
+  return todo
 }
 
 function read(): Array<Todo> {
@@ -37,6 +40,31 @@ function read(): Array<Todo> {
   return db.todos
 }
 
+function update(id: string, partialTodo: Partial<Todo>) {
+  let updatedTodo;
+
+  const todos = read()
+  todos.forEach((currentTodo) => {
+    const isToUpdate = currentTodo.id === id
+
+    if (isToUpdate) {
+      updatedTodo = Object.assign(currentTodo, partialTodo)
+    }
+  })
+
+  if (!updatedTodo) throw new Error("Please, provide another ID.")
+
+  fs.writeFileSync(DB_FILE_PATH, JSON.stringify({ todos }, null, 2))
+}
+
+function updateContentById(id: string, content: string) {
+  return update(
+    id, {
+    content
+  }
+  )
+}
+
 function CLEAR_DB() {
   fs.writeFileSync(DB_FILE_PATH, "")
 }
@@ -44,7 +72,13 @@ function CLEAR_DB() {
 CLEAR_DB()
 create('Primeira TODO')
 create('Segunda TODO')
-create('Terceira TODO')
+const terceiraTodo = create('Terceira TODO')
+
+update(terceiraTodo.id, {
+  content: "novo content da terceira todo",
+  done: true
+})
+
+updateContentById(terceiraTodo.id, 'Atualizando o content novamente')
 console.log(read())
-// console.log(create('Segunda TODO'))
-// console.log(create('Terceira TODO'))
+
